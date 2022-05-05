16f443b8fd70b2bcbc846fdc3185151da03c0f22
viniciusteixeiradias <viniciustdias2001@gmail.com>
2022-05-04 21:25:38 -0300

    Separando arquivos de rota

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const routes = require('./config/routes');
 
 const app = express();
 
@@ -9,17 +10,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(routes);
 
-let db = [
-  { '1': { Nome: 'Cliente 1', Idade: '21', Endereco: 'Rua 1' } },
-  { '2': { Nome: 'Cliente 2', Idade: '22', Endereco: 'Rua 2' } },
-  { '3': { Nome: 'Cliente 3', Idade: '23', Endereco: 'Rua 3' } }
-]
-
-app.get('/', (req, res) => {
-  return res.json(db)
-})
-
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
 
 
 app.listen(3000, () => {
